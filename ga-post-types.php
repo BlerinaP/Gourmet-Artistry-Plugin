102ecb0cdcea84707f4cff459f74c0522b1e1238
BlerinaP <blerina1.p@gmail.com>
2019-12-04 12:50:02 +0100

    Creating a basic taxonomy

--- ga-post-types.php
+++ ga-post-types.php
@@ -1,43 +1,53 @@
 <?php
 /*
- * Plugin Name: Gourmet Artistry Post Types & Taxonomies
- * Plugin URI:
- * Description: Adds Custom Post types to site
- * Version: 1.0
- * Author: Juan De la torre Valdez
- * License: GPL2
- * License: URI: https://www,gnu.org/license/gpl-2.0.html
- */
-
+Plugin Name: Gourmet Artistry Post Types & Taxonomies
+Plugin URI:
+Description: Adds Custom Post Types to site.
+Version: 1.0
+Author: Juan De la torre Valdez
+License: GPL2
+License: URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
 add_action('init', 'ga_recipe_post_type', 0 );
 add_action('init', 'ga_events_post_type', 0 );
+add_action('init', 'meal_type_taxonomy');
 
-function ga_recipe_post_type(){
-    //Labels for the Post Type
+function meal_type_taxonomy(){
+    register_taxonomy(
+        'meal_type', //taxonomy
+        'recipes',
+        array(
+            'label' => __('Meal Type'),
+            'rewrite' => array('slug' => 'meal'),
+            'hierarchical' => true
+        )
+    );
+}
 
+function ga_recipe_post_type() {
+      // Labels for the Post Type
     $labels = array(
-        'name'                => _x( 'Recipes', 'Post Type General Name', 'meals' ),
-        'singular_name'       => _x( 'Recipe', 'Post Type Singular Name', 'meals' ),
-        'menu_name'           => __( 'Recipes', 'meals' ),
-        'parent_item_colon'   => __( 'Parent Recipe', 'meals' ),
-        'all_items'           => __('All Recipes', 'meals'),
-        'view_item'           => __( 'View Recipe', 'meals' ),
-        'add_new_item'        => __( 'Add New Recipe', 'meals' ),
-        'add_new'             => __( 'Add New Recipe', 'meals' ),
-        'edit_item'           => __( 'Edit Recipe','meals' ),
-        'update_item'         => __( 'Update Recipe', 'meals' ),
-        'search-items'        => __( 'Search Recipe', 'meals' ),
-        'not_found'           => __( 'Not Found', 'meals' ),
-        'not_found_in_trash'  => __('Not Found in trash', 'meals' ),
+      'name'                => _x( 'Recipes', 'Post Type General Name', 'gourmet-artist' ),
+      'singular_name'       => _x( 'Recipe', 'Post Type Singular Name', 'gourmet-artist' ),
+      'menu_name'           => __( 'Recipes', 'gourmet-artist' ),
+      'parent_item_colon'   => __( 'Parent Recipe', 'gourmet-artist' ),
+      'all_items'           => __( 'All Recipes', 'gourmet-artist' ),
+      'view_item'           => __( 'View Recipe', 'gourmet-artist' ),
+      'add_new_item'        => __( 'Add New Recipe', 'gourmet-artist' ),
+      'add_new'             => __( 'Add New Recipe', 'gourmet-artist' ),
+      'edit_item'           => __( 'Edit Recipe', 'gourmet-artist' ),
+      'update_item'         => __( 'Update Recipe', 'gourmet-artist' ),
+      'search_items'        => __( 'Search Recipe', 'gourmet-artist' ),
+      'not_found'           => __( 'No recipes found', 'gourmet-artist' ),
+      'not_found_in_trash'  => __( 'Not found in trash', 'gourmet-artist' ),
     );
 
-    //Another Customizations
-
+    // Another Customizations
     $args = array(
-        'label'  => __('Recipes', 'meals' ),
-        'description' => __('Recipes for Gourmet Artistry', 'meals'),
-        'labels' => $labels,
-        'supports' => array('title', 'editor', 'thumbnail', 'revisions'),
+        'label'   => __('Recipes','gourmet-artist' ),
+        'description' => __('Recipes for Gourmet Artistry', 'gourmet-artist'),
+        'labels'  => $labels,
+        'supports' => array('title', 'editor', 'thumbnail','revisions',  ),
         'hierarchical' => false,
         'public' => true,
         'show_ui' => true,
@@ -49,37 +59,39 @@ function ga_recipe_post_type(){
         'can_export' => true,
         'has_archive' => true,
         'exclude_from_search' => false,
-        'capability_type' => 'page'
+        'capability_type' => 'page',
     );
 
-    register_post_type('recipes', $args);
+    // register the post Type
+    register_post_type( 'recipes', $args);
 }
-function ga_events_post_type(){
-    //Labels for the Post Type
 
+
+function ga_events_post_type() {
+
+      // Labels for the Post Type
     $labels = array(
-        'name'                => _x( 'Events', 'Post Type General Name', 'meals' ),
-        'singular_name'       => _x( 'Event', 'Post Type Singular Name', 'meals' ),
-        'menu_name'           => __( 'Events', 'meals' ),
-        'parent_item_colon'   => __( 'Parent Event', 'meals' ),
-        'all_items'           => __('All Events', 'meals'),
-        'view_item'           => __( 'View Event', 'meals' ),
-        'add_new_item'        => __( 'Add New Event', 'meals' ),
-        'add_new'             => __( 'Add New Event', 'meals' ),
-        'edit_item'           => __( 'Edit Event','meals' ),
-        'update_item'         => __( 'Update Event', 'meals' ),
-        'search-items'        => __( 'Search Event', 'meals' ),
-        'not_found'           => __( 'Not Found', 'meals' ),
-        'not_found_in_trash'  => __('Not Found in trash', 'meals' ),
+      'name'                => _x( 'Events', 'Post Type General Name', 'gourmet-artist' ),
+      'singular_name'       => _x( 'Event', 'Post Type Singular Name', 'gourmet-artist' ),
+      'menu_name'           => __( 'Events', 'gourmet-artist' ),
+      'parent_item_colon'   => __( 'Parent Event', 'gourmet-artist' ),
+      'all_items'           => __( 'All Events', 'gourmet-artist' ),
+      'view_item'           => __( 'View Event', 'gourmet-artist' ),
+      'add_new_item'        => __( 'Add New Event', 'gourmet-artist' ),
+      'add_new'             => __( 'Add New Event', 'gourmet-artist' ),
+      'edit_item'           => __( 'Edit Event', 'gourmet-artist' ),
+      'update_item'         => __( 'Update Event', 'gourmet-artist' ),
+      'search_items'        => __( 'Search Event', 'gourmet-artist' ),
+      'not_found'           => __( 'No events found', 'gourmet-artist' ),
+      'not_found_in_trash'  => __( 'Not found in trash', 'gourmet-artist' ),
     );
 
-    //Another Customizations
-
+    // Another Customizations
     $args = array(
-        'label'  => __('Events', 'meals' ),
-        'description' => __('Events for Gourmet Artistry', 'meals'),
-        'labels' => $labels,
-        'supports' => array('title', 'editor' ),
+        'label'   => __('Events','gourmet-artist' ),
+        'description' => __('Events for Gourmet Artistry', 'gourmet-artist'),
+        'labels'  => $labels,
+        'supports' => array('title', 'editor',),
         'hierarchical' => false,
         'public' => true,
         'show_ui' => true,
@@ -91,8 +103,9 @@ function ga_events_post_type(){
         'can_export' => true,
         'has_archive' => true,
         'exclude_from_search' => false,
-        'capability_type' => 'page'
+        'capability_type' => 'page',
     );
 
-    register_post_type('events', $args);
+    // register the post Type
+    register_post_type( 'events', $args);
 }
